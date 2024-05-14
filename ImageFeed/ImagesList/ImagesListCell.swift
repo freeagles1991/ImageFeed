6ed3360da5cf15da12fe408ed5beb93fef1bd749
Freeagles1991 <dmitry.nerovny@gmail.com>
2024-05-14 18:41:10 +0300

    Верстка ячейки таблицы

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Дима on 12.05.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Дима on 03.05.2024.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController{
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = 200
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) // 1
+            
+            guard let imageListCell = cell as? ImagesListCell else { // 2
+                return UITableViewCell()
+            }
+            
+            configCell(for: imageListCell) // 3
+            return imageListCell // 4
+        }
+    
+    func configCell(for cell: ImagesListCell) { }
+    
+    
+}
+
+extension ImagesListViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+}
+
