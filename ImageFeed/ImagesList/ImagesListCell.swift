476c2ad5963c08b4605c7cb0c77198ce2eaf1ca9
Freeagles1991 <dmitry.nerovny@gmail.com>
2024-05-16 16:59:42 +0300

    Динамическая высота ячейки и заполнение изображениями

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -10,4 +10,11 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    
+    @IBOutlet var likeButton: UIButton!
+
+    @IBOutlet var dateLabel: UILabel!
+    
 }
