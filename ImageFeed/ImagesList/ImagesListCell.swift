c3181085cdd708ff8d2a3c82757df01c8884acb0
Freeagles1991 <dmitry.nerovny@gmail.com>
2024-05-24 15:40:27 +0300

    Добавил градиент для изображений

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -17,4 +17,34 @@ final class ImagesListCell: UITableViewCell {
 
     @IBOutlet var dateLabel: UILabel!
     
+    private let gradientLayer = CAGradientLayer()
+    private let gradientHeight: CGFloat = 30.0 // Фиксированная высота градиента
+
+    override func awakeFromNib() {
+         super.awakeFromNib()
+         setupGradientLayer()
+     }
+
+     private func setupGradientLayer() {
+         gradientLayer.colors = [
+             UIColor.clear.cgColor,
+             UIColor.black.withAlphaComponent(0.5).cgColor
+         ]
+         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+         gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+         cellImage.layer.addSublayer(gradientLayer)
+     }
+
+    override func layoutSubviews() {
+            super.layoutSubviews()
+            // Обновляем фрейм градиентного слоя, чтобы он был на дне изображения и имел фиксированную высоту
+            let gradientFrame = CGRect(
+                x: 0,
+                y: cellImage.bounds.height - gradientHeight,
+                width: cellImage.bounds.width,
+                height: gradientHeight
+            )
+            gradientLayer.frame = gradientFrame
+        }
+    
 }
