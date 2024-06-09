0da45f00e911665a145654fbe68ac6561e3c78f8
Freeagles1991 <dmitry.nerovny@gmail.com>
2024-06-09 21:00:22 +0300

    Правки по п3

--- ImageFeed/Services/OAuth2Service.swift
+++ ImageFeed/Services/OAuth2Service.swift
@@ -9,59 +9,67 @@ import Foundation
 
 struct OAuthTokenResponseBody: Decodable {
     let accessToken: String
-
+    
     enum CodingKeys: String, CodingKey {
         case accessToken = "access_token"
     }
     
-    func decodeTokenResponse(with jsonString: String){
-        if let jsonData = jsonString.data(using: .utf8) {
-            do {
-                let authResponse = try JSONDecoder().decode(OAuthTokenResponseBody.self, from: jsonData)
-                print(authResponse)
-            } catch {
-                print("Error decoding JSON: \(error)")
-            }
+    static func decodeTokenResponse(from data: Data) -> Result<String, Error> {
+        do {
+            let response = try JSONDecoder().decode(OAuthTokenResponseBody.self, from: data)
+            return .success(response.accessToken)
+        } catch {
+            return .failure(error)
+        }
     }
+}
     
-
-}
-
-    final class OAuth2Service {
-        static let shared = OAuth2Service()
-        private init() {}
-        
-        private func makeOAuthTokenRequest(code: String) -> URLRequest? {
-            guard let baseURL = URL(string: "https://unsplash.com") else {
-                print("Invalid base URL")
-                return nil
-            }
-            
-            guard let url = URL(
-                string: "/oauth/token"
-                + "?client_id=\(Constants.accessKey)"
-                + "&client_secret=\(Constants.secretKey)"
-                + "&redirect_uri=\(Constants.redirectURI)"
-                + "&code=\(code)"
-                + "&grant_type=authorization_code",
-                relativeTo: baseURL
-            ) else {
-                print("Invalid URL")
-                return nil
-            }
-            
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            return request
-        }
-        
-        func fetchOAuthToken(code: String){
-            guard let request = makeOAuthTokenRequest(code: code) else {
-                print("Invalid request")
-                return }
-            let task = URLSession.shared.dataTask(with: request)
-            task.resume()
-        }
-        
+final class OAuth2Service {
+     static let shared = OAuth2Service()
+     private init() {}
+     
+     private func makeOAuthTokenRequest(code: String) -> URLRequest? {
+         guard let baseURL = URL(string: "https://unsplash.com") else {
+             print("Invalid base URL")
+             return nil
+         }
+         
+         guard let url = URL(
+             string: "/oauth/token"
+             + "?client_id=\(Constants.accessKey)"
+             + "&client_secret=\(Constants.secretKey)"
+             + "&redirect_uri=\(Constants.redirectURI)"
+             + "&code=\(code)"
+             + "&grant_type=authorization_code",
+             relativeTo: baseURL
+         ) else {
+             print("Invalid URL")
+             return nil
+         }
+         
+         var request = URLRequest(url: url)
+         request.httpMethod = "POST"
+         return request
+     }
+     
+     func fetchOAuthToken(code: String, completion: @escaping (Result<String, Error>) -> Void) {
+         guard let request = makeOAuthTokenRequest(code: code) else {
+             completion(.failure(NetworkError.urlSessionError))
+             print("Invalid request")
+             return }
+         
+         let task = URLSession.shared.data(for: request) { result in
+             switch result {
+             case .success(let data):
+                 let decodeResult = OAuthTokenResponseBody.decodeTokenResponse(from: data)
+                 completion(decodeResult)
+                 print("OAuth token received: \(data)")
+             case .failure(let error):
+                 print("Error fetching OAuth token: \(error)")
+                 // Handle error
+             }
+         }
+         
     }
 }
+
