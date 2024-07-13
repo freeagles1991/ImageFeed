8bbdaa2cefcc28315436494725c84c357ec3cb91
Freeagles1991 <dmitry.nerovny@gmail.com>
2024-07-13 21:17:22 +0300

    Перенес управление прогрессом в Presenter

--- ImageFeed/AuthScreen/WebView/WebViewPresenter.swift
+++ ImageFeed/AuthScreen/WebView/WebViewPresenter.swift
@@ -10,6 +10,7 @@ import Foundation
 public protocol WebViewPresenterProtocol {
     var view: WebViewViewControllerProtocol? { get set }
     func loadAuthWebView()
+    func didUpdateProgressValue(_ newValue: Double)
 }
 
 final class WebViewPresenter: WebViewPresenterProtocol {
@@ -32,8 +33,22 @@ final class WebViewPresenter: WebViewPresenterProtocol {
             print("Url не сформирован")
             return
         }
+        
+        self.didUpdateProgressValue(0)
 
         let request = URLRequest(url: url)
         view?.load(request: request)
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+        let newProgressValue = Float(newValue)
+        view?.setProgressValue(newProgressValue)
+        
+        let shouldHideProgress = shouldHideProgress(for: newProgressValue)
+        view?.setProgressHidden(shouldHideProgress)
+    }
+    
+    func shouldHideProgress(for value: Float) -> Bool {
+        abs(value - 1.0) <= 0.0001
     }
 }
--- ImageFeed/AuthScreen/WebView/WebViewViewController.swift
+++ ImageFeed/AuthScreen/WebView/WebViewViewController.swift
@@ -12,6 +12,8 @@ import WebKit
 public protocol WebViewViewControllerProtocol: AnyObject {
     var presenter: WebViewPresenterProtocol? { get set }
     func load(request: URLRequest)
+    func setProgressValue(_ newValue: Float)
+    func setProgressHidden(_ isHidden: Bool)
 }
 
 final class WebViewViewController: UIViewController & WebViewViewControllerProtocol{
@@ -31,7 +33,7 @@ final class WebViewViewController: UIViewController & WebViewViewControllerProto
             options: [],
             changeHandler: { [weak self] _, _ in
                 guard let self = self else { return }
-                self.updateProgress()
+                presenter?.didUpdateProgressValue(webView.estimatedProgress)
             })
     }
     
@@ -45,8 +47,11 @@ final class WebViewViewController: UIViewController & WebViewViewControllerProto
         webView.load(request)
     }
 
-    private func updateProgress() {
-        progressView.progress = Float(webView.estimatedProgress)
-        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    func setProgressValue(_ newValue: Float) {
+        progressView.progress = newValue
+    }
+
+    func setProgressHidden(_ isHidden: Bool) {
+        progressView.isHidden = isHidden
     }
 }
